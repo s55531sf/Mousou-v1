6cd9efa42bebeba064cad8a30ca2380fad1fcab0
s55531sf <free34game@icloud.com>
2022-09-14 19:04:11 +0900

    なにかしたよがんば」

--- Assets/Scenes/SPDup.cs
+++ Assets/Scenes/SPDup.cs
@@ -7,6 +7,7 @@ public class SPDup : MonoBehaviour
 
     public GameObject Bike;
     public GameObject item;
+    public GameObject ouennoyatu;
     private idou spd;
     private Ouen oue;
 
@@ -15,13 +16,13 @@ public class SPDup : MonoBehaviour
         Vector3 aaa = Bike.transform.position;
         float dis = Vector3.Distance(aaa, this.transform.position);
         spd = Bike.GetComponent<idou>();
+        oue = ouennoyatu.GetComponent<Ouen>();
 
         if (dis < 4.6f)
         {
-
             spd.speedup();
-           // oue.ouenflag(1);
             Debug.Log("aaa");
+            oue.ouenflag();
             Destroy(item);
         }
     }
--- Assets/bike/Ouen.cs
+++ Assets/bike/Ouen.cs
@@ -10,6 +10,7 @@ public class Ouen : MonoBehaviour
     public RectTransform ouen3;
     public RectTransform ouen4;
     public RectTransform ouen5;
+    int kazu_o = 0;
 
     void Start()
     {
@@ -19,35 +20,38 @@ public class Ouen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ouen1.position += new Vector3(0.1f, 0, 0);
-        ouen2.position += new Vector3(0.2f, 0, 0);
+        ouen1.position += new Vector3(0.3f, 0, 0);
+        ouen2.position += new Vector3(0.3f, 0, 0);
         ouen3.position += new Vector3(0.3f, 0, 0);
-        ouen4.position += new Vector3(0.4f, 0, 0);
-        ouen5.position += new Vector3(0.5f, 0, 0);
+        ouen4.position += new Vector3(0.3f, 0, 0);
+        ouen5.position += new Vector3(0.3f, 0, 0);
         //ouen1.position = new Vector3(-488, 132, 0);
     }
 
-    public void ouenflag(int kazu_o)
+    public void ouenflag()
     {
-        if (kazu_o == 1)
+        kazu_o += 1;
+        Debug.Log("kazu_o");
+        Debug.Log(kazu_o);
+        if (kazu_o == 2)
         {
-           ouen1.position = new Vector3(-488, 132, 0);
-        }
-        else if (kazu_o == 2)
-        {
-
+           ouen1.position = new Vector3(-40, 370, 92);         
         }
         else if (kazu_o == 3)
         {
-
+            ouen2.position = new Vector3(-90, 400, 92);
         }
         else if (kazu_o == 4)
         {
-
+            ouen3.position = new Vector3(-110, 430, 92);
         }
         else if (kazu_o == 5)
         {
-
+            ouen4.position = new Vector3(-80, 330, 92);
+        }
+        else if (kazu_o == 6)
+        {
+            ouen5.position = new Vector3(-60, 460, 92);
         }
     }
 }
