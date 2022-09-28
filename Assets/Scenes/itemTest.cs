a3c9df4e13a4c13591f7fd81d5592faa287f0363
s55531sf <free34game@icloud.com>
2022-09-28 18:36:50 +0900

    したよ

--- Assets/Scenes/itemTest.cs
+++ Assets/Scenes/itemTest.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class itemTest : MonoBehaviour
 {
     public GameObject Bike;
     public GameObject item;
+    public Text TextFrame;
+    public int DesFlag;
+    int num_o=0;
     //public float speed;
-
 
 
     void Update()
     {
+
+    if (Botan_OR.flag_p != DesFlag && Botan_OB.flag_p != DesFlag)
+        {
+            return;
+
+        }
+
         if (Input.GetKeyDown(KeyCode.Return))
 
         {
+            num_o += 1;
             Shot();
         }
+            TextFrame.text = string.Format("{0} ‰ñ", num_o);
     }
 
     public void Shot()
--- Assets/bike/Ouen.cs
+++ Assets/bike/Ouen.cs
@@ -10,21 +10,31 @@ public class Ouen : MonoBehaviour
     public RectTransform ouen3;
     public RectTransform ouen4;
     public RectTransform ouen5;
+    public RectTransform ouen6;
+    public RectTransform ouen7;
+    public RectTransform ouen8;
+    public RectTransform ouen9;
+    public RectTransform ouen10;
     int kazu_o = 0;
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        ouen1.position += new Vector3(0.3f, 0, 0);
-        ouen2.position += new Vector3(0.3f, 0, 0);
-        ouen3.position += new Vector3(0.3f, 0, 0);
-        ouen4.position += new Vector3(0.3f, 0, 0);
-        ouen5.position += new Vector3(0.3f, 0, 0);
+        ouen1.position += new Vector3(-0.5f, 0, 0);
+        ouen2.position += new Vector3(-0.5f, 0, 0);
+        ouen3.position += new Vector3(-0.5f, 0, 0);
+        ouen4.position += new Vector3(-0.5f, 0, 0);
+        ouen5.position += new Vector3(-0.5f, 0, 0);
+        ouen6.position += new Vector3(-0.5f, 0, 0);
+        ouen7.position += new Vector3(-0.5f, 0, 0);
+        ouen8.position += new Vector3(-0.5f, 0, 0);
+        ouen9.position += new Vector3(-0.5f, 0, 0);
+        ouen10.position += new Vector3(-0.5f, 0, 0);
         //ouen1.position = new Vector3(-488, 132, 0);
     }
 
@@ -33,26 +43,52 @@ public class Ouen : MonoBehaviour
         kazu_o += 1;
         Debug.Log("kazu_o");
         Debug.Log(kazu_o);
-        if (kazu_o == 2)
+        if (kazu_o == 1)
         {
-           ouen1.position = new Vector3(-40, 370, 92);         
+            ouen1.position = new Vector3(2200, 300, 92);
+        }
+        else if (kazu_o == 2)
+        {
+            ouen2.position = new Vector3(2200, 400, 92);
         }
         else if (kazu_o == 3)
         {
-            ouen2.position = new Vector3(-90, 400, 92);
+            ouen3.position = new Vector3(2200, 500, 92);
         }
         else if (kazu_o == 4)
         {
-            ouen3.position = new Vector3(-110, 430, 92);
+            ouen4.position = new Vector3(2200, 600, 92);
         }
         else if (kazu_o == 5)
         {
-            ouen4.position = new Vector3(-80, 330, 92);
+            ouen5.position = new Vector3(2200, 800, 92);
+
         }
         else if (kazu_o == 6)
         {
-            ouen5.position = new Vector3(-60, 460, 92);
+            ouen6.position = new Vector3(2200, 700, 92);
+
+        }
+        else if (kazu_o == 7)
+        {
+            ouen7.position = new Vector3(2200, 400, 92);
+
+        }
+        else if (kazu_o == 8)
+        {
+            ouen8.position = new Vector3(2200, 900, 92);
+
+        }
+        else if (kazu_o == 9)
+        {
+            ouen9.position = new Vector3(2200, 1000, 92);
+
+        }
+        else if (kazu_o == 10)
+        {
+            ouen10.position = new Vector3(2200, 300, 92);
             kazu_o = 1;
         }
+
     }
 }
